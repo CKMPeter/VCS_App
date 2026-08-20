3bd9a2f9b35b966649fea3b1e52f5fe1c3c96086
unknown <caokhaiminh7903@gmail.com>
2026-08-21 05:10:11 +0700

    fix router bug

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import './App.css';
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { MainPage } from './components/mainPage';
-import { Checkin } from './components/checkin';
-import { Table } from './components/table';
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { MainPage } from "./components/mainPage";
+import { Checkin } from "./components/checkin";
+import { Table } from "./components/table";
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/checkin" element={<Checkin />} />
